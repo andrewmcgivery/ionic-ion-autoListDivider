2fb2bde15cf68d47620b60d5d1d64dfa2997c1a6
andrewmcgivery <andrew@mcgivery.com>
2015-02-04 19:03:22 -0500

    Update ionic-ion-autoListDivider.js
    Fixed isolate scope issue

--- ionic-ion-autoListDivider.js
+++ ionic-ion-autoListDivider.js
@@ -1,30 +1,29 @@
 angular.module('ionic.ion.autoListDivider',[])
 
-.directive('autoListDivider', function() {  
+.directive('autoListDivider', function($timeout) {  
 	var lastDivideKey = "";
 
 	return {
-		scope: {
-			autoListDividerFunction: "="
-		}
 		link: function(scope, element, attrs) {
-			var index = scope.$index;
-
 			var key = attrs.autoListDividerValue;
 
 			var defaultDivideFunction = function(k){
 				return k.slice( 0, 1 ).toUpperCase();
 			}
+      
+			var doDivide = function(){
+				var divideFunction = scope.$apply(attrs.autoListDividerFunction) || defaultDivideFunction;
+				var divideKey = divideFunction(key);
+				
+				if(divideKey != lastDivideKey) { 
+					var contentTr = angular.element("<div class='item item-divider'>"+divideKey+"</div>");
+					element[0].parentNode.insertBefore(contentTr[0], element[0]);
+				}
 
-			var divideFunction = scope.autoListDividerFunction || defaultDivideFunction;
-			var divideKey = divideFunction(key);
-
-			if(divideKey != lastDivideKey) { 
-				var contentTr = angular.element("<div class='item item-divider'>"+divideKey+"</div>");
-				element[0].parentNode.insertBefore(contentTr[0], element[0]);
+				lastDivideKey = divideKey;
 			}
-
-			lastDivideKey = divideKey;
+		  
+			$timeout(doDivide,0)
 		}
 	}
 });
